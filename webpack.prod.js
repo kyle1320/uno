fe6118b3101a43bab092c3aaed3945563c7a789b
Kyle Cutler <kyle1320@gmail.com>
2020-07-18 18:33:24 -0700

    Add webpack prod config

---
+++ webpack.prod.js
@@ -0,0 +1,93 @@
+const path = require('path');
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const template = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Play Uno</title>
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>`;
+
+const client = {
+  mode: 'production',
+  entry: {
+    homepage: './src/client/homepage.tsx',
+    gameroom: './src/client/gameroom.tsx'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      { test: /\.tsx?$/, use: 'ts-loader' },
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks: ['homepage'],
+      filename: 'index.html',
+      templateContent: template
+    }),
+    new HtmlWebpackPlugin({
+      chunks: ['gameroom'],
+      filename: 'gameroom.html',
+      templateContent: template
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist/public'),
+    publicPath: '/'
+  }
+};
+
+const server = {
+  mode: 'development',
+  entry: './src/server/index.ts',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      { test: /\.ts$/, use: 'ts-loader' }
+    ]
+  },
+  target: 'node',
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  externals: [nodeExternals()],
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: 'require("source-map-support").install();',
+      raw: true,
+      entryOnly: false
+    })
+  ],
+  output: {
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'dist'),
+  }
+};
+
+module.exports = [client, server];
