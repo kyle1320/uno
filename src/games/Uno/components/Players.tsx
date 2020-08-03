93e10a613c63674c433971c82f6a0cabe19ff314
Kyle Cutler <kyle1320@gmail.com>
2020-08-02 18:50:34 -0700

    Show hint when there are no other players

--- src/games/Uno/components/Players.tsx
+++ src/games/Uno/components/Players.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClone } from '@fortawesome/free-solid-svg-icons';
 
 import { UnoSpec } from '..';
 import { state } from '../../../types';
@@ -12,17 +14,46 @@ interface IProps {
   relativeTurnOrder: string[];
 }
 
+function getRoomLink() {
+  return `${location.protocol}//${location.hostname}${location.pathname}`;
+}
+
+function copyRoomLink() {
+  navigator.clipboard.writeText(getRoomLink());
+}
+
 export function Players(props: IProps) {
+  const [copied, setCopied] = React.useState(false);
+  const copy = React.useCallback(() => {
+    copyRoomLink();
+    setCopied(true);
+  }, [setCopied]);
+
   return (
     <div className="players">
-      {props.relativeTurnOrder.map((id, i) =>
-        i > 0 ? (
-          <Player
-            key={id}
-            id={id}
-            placement={i / props.relativeTurnOrder.length}
-          />
-        ) : null
+      {props.relativeTurnOrder.length > 1 ? (
+        props.relativeTurnOrder.map((id, i) =>
+          i > 0 ? (
+            <Player
+              key={id}
+              id={id}
+              placement={i / props.relativeTurnOrder.length}
+            />
+          ) : null
+        )
+      ) : (
+        <div className="no-players">
+          <div className="no-players-heading">Nobody Else is Here</div>
+          <div className="no-players-subheading">
+            invite others to play using this link{copied ? ' (copied)' : ''}:
+          </div>
+          <div className="no-players-link-wrapper">
+            <a href="#" className="no-players-link">
+              {getRoomLink()}
+            </a>
+            <FontAwesomeIcon icon={faClone} onClick={copy} title="Copy Link" />
+          </div>
+        </div>
       )}
     </div>
   );
