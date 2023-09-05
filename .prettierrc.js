c0a855ee632683af948e53830e90f7a9cebf1c70
Kyle Cutler <kyle1320@gmail.com>
2023-09-04 17:33:53 -0700

    Update formatting

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,3 @@
 module.exports = {
-  semi: true,
-  singleQuote: true,
-  printWidth: 80,
-  tabWidth: 2,
-  bracketSpacing: true,
-  jsxBracketSameLine: true,
-  arrowParens: "avoid",
-  trailingComma: "none"
+  trailingComma: 'none'
 };
